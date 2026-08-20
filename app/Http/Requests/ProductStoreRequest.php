49863e5919ec0630373784962de5ed6e9ad82ffc
Lerior <leonardogarcia3500@gmail.com>
2026-08-19 19:20:23 -0600

    feat: add ProductStoreRequest

---
+++ app/Http/Requests/ProductStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:100'],
+            'price' => ['required', 'decimal:0,2', 'min:0'],
+            'description' => ['nullable', 'string'],
+            'stock' => ['required', 'integer', 'min:0'],
+            'images' => ['required', 'array', 'min:1', 'max:10'],
+            'images.*' => [
+                'image',
+                'mimes:png,jpg,jpeg,webp',
+                'max:5120',
+            ]
+        ];
+    }
+}
